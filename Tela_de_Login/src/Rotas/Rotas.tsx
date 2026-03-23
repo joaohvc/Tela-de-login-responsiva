d5a23b63e0144bae07351552cad9ed658e7b01f4
Joao carmo <jhenrique.viana2007@gmail.com>
2026-03-23 14:29:27 -0300

    Removendo linhas desnecessarias

--- Tela_de_Login/src/Rotas/Rotas.tsx
+++ Tela_de_Login/src/Rotas/Rotas.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import LOGIN from "../Login/Login"
+
 import CADASTRO from "../Cadastro/Cadastro"
 import LOGIN2 from "../Login/login2"
 
@@ -7,7 +7,7 @@ function ROTAS () {
     return(
         <div>
             <Routes>
-                <Route path="/Login2" element={<LOGIN/>}/>
+                
                 <Route path="/Cadastra-se" element={<CADASTRO/>}/>
                 <Route path="/" element={<LOGIN2/>}/>
             </Routes>

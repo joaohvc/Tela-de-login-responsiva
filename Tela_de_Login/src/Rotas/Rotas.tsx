c60987c4fe8e3cdea98ed7d74f32c2f948d0508d
Joao carmo <jhenrique.viana2007@gmail.com>
2026-03-24 12:20:21 -0300

    Tela de cadastro responsiva parte01

--- Tela_de_Login/src/Rotas/Rotas.tsx
+++ Tela_de_Login/src/Rotas/Rotas.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 
-import CADASTRO from "../Cadastro/Cadastro"
+
 import LOGIN2 from "../Login/login2"
+import CADASTRO2 from "../Cadastro/Cadastro2"
 
 function ROTAS () {
     return(
         <div>
             <Routes>
                 
-                <Route path="/Cadastra-se" element={<CADASTRO/>}/>
+                
+                <Route path="/Cadastra-se" element={<CADASTRO2/>}/>
                 <Route path="/" element={<LOGIN2/>}/>
             </Routes>
         </div>

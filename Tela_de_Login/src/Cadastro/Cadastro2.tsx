d5c695ffc87a8e2649d3d1e886c38259692d76ca
Joao carmo <jhenrique.viana2007@gmail.com>
2026-03-24 13:43:44 -0300

    finalizando o projeto so no Front-End

--- Tela_de_Login/src/Cadastro/Cadastro2.tsx
+++ Tela_de_Login/src/Cadastro/Cadastro2.tsx
@@ -3,57 +3,56 @@ import { Link } from "react-router-dom";
 function CADASTRO2() {
   return (
     <div className="min-h-screen h-screen overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
-        <div className="flex flex-col w-screen h-screen items-center mt-5 lg:flex-row-reverse lg:justify-center">
-      <div className=" bg-white w-90 h-170 shadow-2xl rounded-t-2xl lg:h-150 lg:w-100 lg:rounded-r-2xl ">
-        <div className="flex flex-col items-center justify-center h-170 mt-10 lg:mt-0">
-          <h1 className="text-3xl relative bottom-30">Cadastrar-se</h1>
-          <h1 className="">EMAIL</h1>
-          <input
-            type="text"
-            placeholder="SeuEmail@gmail.com"
-            className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5"
-          />
-          <h1>CPF</h1>
-          <input
-            type="text"
-            placeholder="SeuCPF"
-            className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5"
-          />
-          <h1>SENHA</h1>
-          <input
-            type="password"
-            placeholder="SuaSenha"
-            className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5"
-          />
-          <h1>Data de Nascimento</h1>
-          <input
-            type="number"
-            placeholder="00/00/0000"
-            className="bg-gray-300 rounded-2xl w-80 h-8 pl-5"
-          />
-          <button className="bg-blue-500 text-white w-40 h-10 rounded-2xl relative top-10 hover:bg-blue-600 text-xl lg:">
-            Entrar
-          </button>
+      <div className="flex flex-col w-screen h-screen items-center mt-5 lg:mt-0 lg:flex-row-reverse lg:justify-center">
+        <div className=" bg-white w-90 h-170 shadow-2xl rounded-t-2xl lg:h-110 lg:w-100 lg:rounded-r-2xl ">
+          <div className="flex flex-col items-center justify-center h-170 mt-10 lg:h-110">
+            <h1 className="text-3xl relative bottom-15">Cadastrar-se</h1>
+            <h1 className="">EMAIL</h1>
+            <input
+              type="text"
+              placeholder="SeuEmail@gmail.com"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5 lg:mb-0"
+            />
+            <h1>CPF</h1>
+            <input
+              type="text"
+              placeholder="SeuCPF"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5 lg:mb-0 "
+            />
+            <h1>SENHA</h1>
+            <input
+              type="password"
+              placeholder="SuaSenha"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5 lg:mb-0 "
+            />
+            <h1>Data de Nascimento</h1>
+            <input
+              type="number"
+              placeholder="00/00/0000"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5"
+            />
+            <button className="bg-blue-500 text-white w-40 h-10 rounded-2xl relative top-5 hover:bg-blue-600 text-xl lg:cursor-pointer">
+              Entrar
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="bg-blue-500 flex w-90 h-40 rounded-b-2xl shadow-2xl lg:h-150 lg:w-60 lg:rounded-l-2xl lg:rounded-bl-2xl lg:rounded-br-none">
-        <div className="flex flex-col h-40 w-90 lg:w-60 lg:h-150">
-          <h1 className="text-white m-5">
-            Caso você já tenha um Cadastro aperte no botão abaixo para fazer o
-            login
-          </h1>
-          <div className="flex justify-center lg:w-60 lg:h-150">
-            <Link to="/">
-              <button className="bg-white w-40 h-10 rounded-2xl hover:bg-gray-300 text-xl">
-                Logar
-              </button>
-            </Link>
+        <div className="bg-blue-500 flex w-90 h-40 rounded-b-2xl shadow-2xl lg:h-110 lg:w-60 lg:rounded-l-2xl lg:rounded-bl-2xl lg:rounded-br-none">
+          <div className="flex flex-col h-40 w-90 lg:w-60 lg:h-150">
+            <h1 className="text-white m-5">
+              Caso você já tenha um Cadastro aperte no botão abaixo para fazer o
+              login
+            </h1>
+            <div className="flex justify-center lg:w-60">
+              <Link to="/">
+                <button className="bg-white w-40 h-10 rounded-2xl cursor-pointer hover:bg-gray-300 text-xl">
+                  Logar
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    
   );
 }
 
--- Tela_de_Login/src/Login/login2.tsx
+++ Tela_de_Login/src/Login/login2.tsx
@@ -3,51 +3,42 @@ import { Link } from "react-router-dom";
 function LOGIN2() {
   return (
     <div className="min-h-screen h-screen overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
-      {/*div que centraliza a tela de login no meu tela*/}
-      <div className="h-screen w-screen flex items-center justify-center ">
-        {/*div que cria o bloco azul da tela de login*/}
-        <div className="bg-blue-500 h-80 w-35 rounded-l-2xl sm:h-100 sm:w-50 lg:h-110 lg:w-70 2xl:w-70 2xl:h-150">
-          {/*div que vai orginazar a informações dentro do bloco azul*/}
-          <div className="flex flex-col items-center">
-            <p className="text-white text-sm mb-10 mr-2 ml-2 mt-4 sm:text-xl lg:text-2xl 2xl:text-3xl ">
-              Caso você não tenho <br />
-              se castrado ainda <br /> para realizar o login,
-              <br /> por favor apertar no botão "Cadastra-se" para realizar seu
-              cadastro.
-            </p>
-            <Link to="/Cadastra-se">
-              <button className="bg-white w-26 h-8 relative rounded-2xl cursor-pointer hover:bg-blue-600 sm:w-36 sm:h-13 sm:text-xl  lg:w-45 lg:h-16 lg:text-xl 2xl:w-50 2xl:h-18 2xl:text-3xl 2xl:top-10 ">
-                Cadastrar-se
-              </button>
-            </Link>
-          </div>
-        </div>
-        {/*div que cria o bloco branco da tela de login*/}
-        <div className="bg-white h-80 w-60 rounded-r-2xl shadow-2xl sm:h-100 sm:w-90 lg:h-110 lg:w-130 2xl:w-150 2xl:h-150 ">
-          {/*div que vai orginazar a informações dentro do bloco branco*/}
-          <div className="flex flex-col items-center h-80 justify-center sm:h-100 lg:h-110 2xl:h-150">
-            <h1 className="relative bottom-15 text-2xl sm:text-4xl lg:text-6xl">
-              Login
-            </h1>
-            <h1 className="relative right-16 sm:right-30 sm:text-xl lg:right-45 lg:text-2xl 2xl:right-60 2xl:text-2xl">
-              Email
-            </h1>
+      <div className="flex flex-col w-screen h-screen items-center mt-5 lg:mt-0 lg:flex-row-reverse lg:justify-center">
+        <div className=" bg-white w-90 h-170 shadow-2xl rounded-t-2xl lg:h-110 lg:w-100 lg:rounded-r-2xl ">
+          <div className="flex flex-col items-center justify-center h-170 mt-10 lg:h-110">
+            <h1 className="text-3xl relative bottom-15">Cadastrar-se</h1>
+            <h1 className="">EMAIL</h1>
             <input
               type="text"
               placeholder="SeuEmail@gmail.com"
-              className="mb-3 bg-gray-300 rounded-2xl h-8 pl-4 sm:h-12 sm:w-80 lg:w-110 lg:h-12 lg:text-2xl 2xl:w-140"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5 lg:mb-5"
             />
-            <h1 className="relative right-16 sm:right-30 sm:text-xl lg:right-45 lg:text-2xl 2xl:right-60 2xl:text-2xl">
-              Senha
-            </h1>
+            
+            <h1>SENHA</h1>
             <input
               type="password"
               placeholder="SuaSenha"
-              className="bg-gray-300 rounded-2xl h-8 pl-4 sm:h-12 sm:w-80 lg:w-110 lg:h-12 lg:text-2xl 2xl:w-140"
+              className="bg-gray-300 rounded-2xl w-80 h-8 pl-5 mb-5 lg:mb-5 "
             />
-            <button className="relative top-9 bg-blue-500 h-8 w-30 rounded-2xl hover:bg-blue-600 cursor-pointer sm:w-36 sm:h-13 sm:text-xl sm:top-14 lg:w-45 lg:h-16 lg:text-2xl 2xl:w-50 2xl:h-18 2xl:text-3xl 2xl:top-19">
-              Logar
+            <button className="text-blue-400 hover:text-blue-600 cursor-pointer relative right-20 lg:right-0">Esqueceu sua senha?</button>
+            
+            <button className="bg-blue-500 text-white w-40 h-10 rounded-2xl relative top-5 hover:bg-blue-600 text-xl lg:cursor-pointer">
+              Entrar
             </button>
+          </div>
+        </div>
+        <div className="bg-blue-500 flex w-90 h-40 rounded-b-2xl shadow-2xl lg:h-110 lg:w-60 lg:rounded-l-2xl lg:rounded-bl-2xl lg:rounded-br-none">
+          <div className="flex flex-col h-40 w-90 lg:w-60 lg:h-150">
+            <h1 className="text-white m-5">
+              Caso você não tenha uma conta ainda, aperta no botão abaixo para criar sua conta
+            </h1>
+            <div className="flex justify-center lg:w-60">
+              <Link to="/Cadastra-se">
+                <button className="bg-white w-40 h-10 rounded-2xl cursor-pointer hover:bg-gray-300 text-xl">
+                  Cadastrar-se
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
